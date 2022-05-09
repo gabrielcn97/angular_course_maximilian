1227df6b9fd61b8239c5939d567c8d7abe9d03d5
gabrielcn97 <cabreranavarrogabrielernesto@gmail.com>
2022-05-09 12:09:57 -0400

    Obtener el índice al usar ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   secretpassword = false;
-  log: number[] = [];
+  log: Date[] = [];
 
   passwordSee(event : Event){
     this.secretpassword = !this.secretpassword;
-    this.log.push(this.log.length + 1);
+    /* this.log.push(this.log.length + 1); */
+    //Utilizando Timestamp
+    this.log.push(new Date());
   }
 
   }
