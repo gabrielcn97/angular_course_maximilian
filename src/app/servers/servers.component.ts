6b6121481306c8642cdd00c49763b37b57aa34c2
gabrielcn97 <cabreranavarrogabrielernesto@gmail.com>
2022-05-09 09:21:58 -0400

    Uso de ngIf para generar datos condicionalmente

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -24,11 +25,13 @@ export class ServersComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onCreateServer(){
-    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
-  onUpdateServerName(event: Event){
-    this.serverName=(<HTMLInputElement>event.target).value;
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
